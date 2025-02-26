7818d6d9a4250460881e9c9b47a17da772bd9510
Massa Timothée <timothee.massa.perso@gmail.com>
2025-02-26 19:17:33 +0100

    feat: create basic genetic algorithm functions

--- src/tsp_genetic.py
+++ src/tsp_genetic.py
@@ -8,8 +8,36 @@ class TSPGenetic:
         self.generations = generations
         self.mutation_rate = mutation_rate
         self.cities = np.random.rand(num_cities, 2)
+        self.distance_matrix = self.calculate_distance_matrix()
         self.population = self.generate_population()
 
+    def calculate_distance_matrix(self):
+        # Initialize distance matrix
+        distance_matrix = np.zeros((self.num_cities, self.num_cities))
+        for i in range(self.num_cities):
+            for j in range(self.num_cities):
+                if i != j:
+                    distance_matrix[i, j] = np.linalg.norm(self.cities[i] - self.cities[j])
+        return distance_matrix
+
     def generate_population(self):
-        return [random.sample(range(self.num_cities), self.num_cities) for _ in range(self.population_size)]
+        population = []
+        for _ in range(self.population_size):
+            # Each individual is a permutation of the cities
+            individual = np.random.permutation(self.num_cities)
+            population.append(individual)
+        return population
+
+    def fitness(self, individual, distance_matrix):
+        total_distance = 0
+        for i in range(len(individual)):
+            from_city = individual[i]
+            to_city = individual[(i + 1) % len(individual)]
+            total_distance += distance_matrix[from_city, to_city]
+        return 1 / total_distance
     
+    def selection(self):
+        sorted_population = sorted(self.population, key=lambda x: self.fitness(x, self.distance_matrix), reverse=True)
+        # Keep the top half of the population
+        sorted_population = sorted_population[:self.population_size // 2]
+        return sorted_population
