ded8ec5cbcdd4e2aec7987c700d810ba49855b99
Massa Timothée <timothee.massa.perso@gmail.com>
2025-03-09 23:04:48 +0100

    feat: add test for Kruskal and Prim algo

--- tests/test_tsp.py
+++ tests/test_tsp.py
@@ -1,43 +1,31 @@
 import pytest
 import numpy as np
 import time
-from src.tsp_genetic import TSPGenetic
-from src.tsp_genetic_old import TSPGenetic
+from src.tsp_algo import TSPKruskal, TSPPrim
 
-def run_tsp_test(num_cities, population_size, gens, mutation_rate, elitism, reps_count):
-    pre_gen_cities = np.random.rand(num_cities, 2)
-    tsp1_time, tsp1_score = 0, 0
-    tsp2_time, tsp2_score = 0, 0
+def test_tsp_kruskal_speed():
+    num_cities = 100
+    cities = np.random.rand(num_cities, 2)
+    tsp = TSPKruskal(num_cities, cities)
 
-    for _ in range(reps_count):
-        tsp1 = TSPGenetic(num_cities, population_size, gens, mutation_rate, elitism, pre_gen_cities=pre_gen_cities)
-        tsp2 = TSPGenetic(num_cities, population_size, gens, mutation_rate, elitism, pre_gen_cities=pre_gen_cities)
-        
-        t1 = time.time()
-        tsp1.run()
-        tsp1_time += time.time() - t1
-        tsp1_score += tsp1.best_distance
+    start_time = time.time()
+    tsp.run()
+    end_time = time.time()
 
-        t1 = time.time()
-        tsp2.run()
-        tsp2_time += time.time() - t1
-        tsp2_score += tsp2.best_distance
-    
-    return {
-        "tsp1_avg_time": tsp1_time / reps_count,
-        "tsp1_avg_score": tsp1_score / reps_count,
-        "tsp2_avg_time": tsp2_time / reps_count,
-        "tsp2_avg_score": tsp2_score / reps_count,
-    }
+    elapsed_time = end_time - start_time
+    assert elapsed_time < 1
 
-@pytest.mark.parametrize("num_cities, population_size, generations, mutation_rate, elitism, reps_count", [
-    (100, 100, 50, 0.03, 0.1, 10)
-])
-def test_tsp(num_cities, population_size, generations, mutation_rate, elitism, reps_count):
-    results = run_tsp_test(num_cities, population_size, generations, mutation_rate, elitism, reps_count)
-    
-    print(f"TSP_cur {generations} gens: avg_time={results['tsp1_avg_time']}; avg_score={results['tsp1_avg_score']}")
-    print(f"TSP_nex {generations} gens: avg_time={results['tsp2_avg_time']}; avg_score={results['tsp2_avg_score']}")
-    
-    assert results['tsp1_avg_score'] > 0, "TSP1 average score should be positive"
-    assert results['tsp2_avg_score'] > 0, "TSP2 average score should be positive"
+def test_tsp_prim_speed():
+    num_cities = 100
+    cities = np.random.rand(num_cities, 2)
+    tsp = TSPPrim(num_cities, cities)
+
+    start_time = time.time()
+    tsp.run()
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    assert elapsed_time < 1  
+
+if __name__ == "__main__":
+    pytest.main()
